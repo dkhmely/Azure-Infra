9a160a3b437873528b90d0978856e1cf76833142
Denys Khmelnytskyi <khmelnytskyi.denys@gmail.com>
2025-09-24 20:06:06 +0200

    typo kv name

--- IaC/data.tf
+++ IaC/data.tf
@@ -5,7 +5,7 @@ data "azurerm_resource_group" "rg" {
 }
 
 data "azurerm_key_vault" "kv" {
-  name                = "${var.application}-${var.env}-kv"
+  name                = "${var.application}${var.env}kv"
   resource_group_name = data.azurerm_resource_group.rg.name
 }
 

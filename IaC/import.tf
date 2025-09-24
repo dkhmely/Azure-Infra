342d127f32f304fa3d0783134d4ea109b50d1385
Denys Khmelnytskyi <khmelnytskyi.denys@gmail.com>
2025-09-24 22:13:09 +0200

    rm import

--- IaC/import.tf
+++ IaC/import.tf
@@ -1,14 +1,4 @@
 import {
-  id = "/subscriptions/70612abe-819d-4e7f-8b16-ef74e921f18b/resourceGroups/voter-dev-rg/providers/Microsoft.Network/virtualNetworks/voter-dev-vnet/subnets/voter-dev-subnet"
-  to = azurerm_subnet.webapp_subnet
-}
-
-import {
-  id = "/subscriptions/70612abe-819d-4e7f-8b16-ef74e921f18b/resourceGroups/voter-dev-rg/providers/Microsoft.Network/virtualNetworks/voter-dev-vnet/subnets/voter-dev-sql-subnet"
-  to = azurerm_subnet.sql_subnet
-}
-
-import {
-  id = "/subscriptions/70612abe-819d-4e7f-8b16-ef74e921f18b/resourceGroups/voter-dev-rg/providers/Microsoft.Network/virtualNetworks/voter-dev-vnet/subnets/voter-dev-pep"
-  to = azurerm_subnet.pep_subnet
+  id = ""
+  to = 
 }
